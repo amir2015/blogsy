9168fa961d8981598021985a28c79efb0374b6ad
amir2015 <zahranovich2@gmail.com>
2020-10-15 23:15:28 +0200

    all functionng well

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201014155727) do
+ActiveRecord::Schema.define(version: 20201015203538) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
