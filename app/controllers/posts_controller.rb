4f999a5ee2c720c6726c6c8f611ae2b40b3b4c88
amir2015 <zahranovich2@gmail.com>
2020-10-15 16:28:18 +0200

    crud running

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,24 @@ def show
   @post = Post.find(params[:id])
 end
 
+  def update 
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+    redirect_to @post
+    else
+    render  'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
   
   private
 
